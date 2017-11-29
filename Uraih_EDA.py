f7230f486f331305ccdc100ca879b9ca7b70fdd4
Chukwudi Uraih <chukwudi.uraih@gmail.com>
2017-11-29 15:28:11 -0500

    WED update

--- Uraih_EDA.py
+++ Uraih_EDA.py
@@ -1,83 +1,166 @@
 
 # coding: utf-8
 
-# In[3]:
+# In[1]:
 
 import pandas as pd
 import json
 import csv
 
 
-# In[4]:
+# In[2]:
 
 data = pd.read_json('https://s3.amazonaws.com/temp-data-pulls/newdump.json')
 
 
-# In[10]:
-
-csv = data.to_csv()
-
-
-# In[5]:
+# In[ ]:
 
 data
 
 
-# In[6]:
+# In[3]:
 
 data['type'].value_counts()
 
 
-# In[26]:
+# In[4]:
 
 instagram_data_pic = data[(data['type']  == 'instagram pic')]
 instagram_data_vid = data[(data['type']  == 'instagram vid')]                   
 instagram_data = instagram_data_pic.append(instagram_data_vid)
 
 
-# In[27]:
-
-instagram_data.shape
-
-
-# In[15]:
+# In[5]:
 
 faceboook_data = data[(data['type']  == 'facebook post')]
 
 
-# In[28]:
-
-faceboook_data.shape
-
-
-# In[34]:
+# In[ ]:
 
 instagram_data.sort_values(by='engagement', ascending=0).head()
 
 
-# In[35]:
+# In[6]:
 
-faceboook_data.sort_values(by='engagement', ascending=0).head()
+c.sort_values(by='engagement', ascending=0).head()
 
 
-# In[37]:
+# In[96]:
+
+# Review facebook data types
+faceboook_data.info()
+
+
+# In[7]:
 
 faceboook_data['brand'].value_counts()
 
 
-# In[39]:
+# In[8]:
 
 instagram_data['brand'].value_counts()
 
 
+# In[99]:
+
+combo_data = instagram_data.append(faceboook_data)
+
+
+# In[81]:
+
+combo_data.to_csv('combo_data.csv', sep=',')
+
+
+# In[100]:
+
+combo_data.describe()
+
+
+# In[131]:
+
+# Combine instagram and facebook data
+combo_data.head(10)
+
+
 # In[ ]:
 
-faceboook_data.descrbe()
+combo_data.c
+
+
+# In[102]:
+
+# Split up the 'channel info' column which is a series of dictionaries
+combo_data['channel_type'] = [x['type'] for x in combo_data['channel_info']]
+combo_data['channel'] = [x['channel'] for x in combo_data['channel_info']]
+
+# Drop the channel info column now that we have split it into two new columns
+combo_data.drop('channel_info', axis = 1, inplace=True)
+
+
+# In[103]:
+
+combo_data['channel_type'].head()
+
+
+# In[104]:
+
+# Remove brackets from from elements in column 
+combo_data['channel_type'] = combo_data['channel_type'].apply(lambda x: x[0])
+
+
+# In[105]:
+
+combo_data['channel_type'].head()
+
+
+# In[106]:
+
+# Create csv of data
+combo_data.to_csv('combo_data.csv')
+
+
+# In[107]:
+
+# Review column names
+combo_data.columns
 
 
 # In[ ]:
 
-instagram_data.describe()
+# Going back and update facebook df
+
+
+# In[141]:
+
+# Group  by brand and channel type for IG and facebook
+brand_channel = combo_data.groupby('brand').channel_type.value_counts()
+brand_channel = pd.DataFrame(brand_channel)
+
+brand_channel
+
+
+# In[143]:
+
+# Split up the 'channel info' column which is a series of dictionaries
+faceboook_data['channel_type'] = [x['type'] for x in faceboook_data['channel_info']]
+faceboook_data['channel'] = [x['channel'] for x in faceboook_data['channel_info']]
+
+# Drop the channel info column now that we have split it into two new columns
+faceboook_data.drop('channel_info', axis = 1, inplace=True)
+
+
+# In[144]:
+
+faceboook_data['channel_type'] = faceboook_data['channel_type'].apply(lambda x: x[0])
+
+
+# In[146]:
+
+# Group  by brand and channel type for IG and facebook
+brand_channel_f = faceboook_data.groupby('brand').channel_type.value_counts()
+brand_channel_f = pd.DataFrame(brand_channel)
+
+brand_channel_f
 
 
 # In[ ]:
