780a65db62d62906f859669683618aa01d4fe0cb
bpshaver <benpshaver@gmail.com>
2017-11-29 17:01:02 -0500

    Consolidated Steves work today

--- 00_Importing_Data.py
+++ 00_Importing_Data.py
@@ -32,7 +32,47 @@ data.drop('channel_info', axis = 1, inplace=True)
 
 #%%
 # Filter only instagram and facebook data
-FB_and_IG_data = data.loc[(data['channel'] == 'facebook') | (data['channel'] == 'instagram')]
+data = data.loc[(data['channel'] == 'facebook') | (data['channel'] == 'instagram')]
 
 #%%
-FB_and_IG_data.to_csv('assets/FB_and_IG_data.csv')
+# Check and make sure every row in the 'channel type' series has exacly 1 element
+assert data['channel_type'].apply(lambda x: len(x) != 1).sum() == 0
+
+data['channel_type'] = data['channel_type'].apply(lambda x: x[0])
+
+#%%
+
+brands = {137314 : 'Conde_Naste_Traveler', 
+          137329 : 'W_Magazine',
+          137321 : 'Onself',
+          137325 : 'Vanity_Fair', 
+          137300 : 'Clever', 
+          137322 : 'Teen_Vogue', 
+          137299 : 'Allure', 
+          137326 : 'Vogue',137316 : 'Glamor'
+         }
+
+data['brand_name'] = data['brand'].map(brands)      
+
+
+#%%
+instagram = data[data['channel'] == 'instagram']
+
+
+#%%
+
+instagram = instagram.reset_index(drop=True)
+
+
+# Turning post_id into urls
+
+
+instagram['post_id'] = [x['post_id'] for x in instagram['content']]
+
+instagram['urls'] = 'http://instdrive.com/p/' + instagram['post_id'].astype(str)
+
+instagram.drop(['post_id'], axis=1, inplace=True)
+
+
+#%%
+data.to_csv('assets/FB_and_IG_data.csv')
