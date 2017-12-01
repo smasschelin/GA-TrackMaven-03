1550503d9f51c7a424bc1718cb0a6992bca0297d
bpshaver <benpshaver@gmail.com>
2017-12-01 18:22:17 -0500

    FB Cleaning

--- 00_Importing_Data.py
+++ 00_Importing_Data.py
@@ -60,7 +60,7 @@ data['brand_name'] = data['brand'].map(brands)
 instagram = data[data['channel'] == 'instagram']
 instagram = instagram.reset_index(drop=True)
 facebook  = data[data['channel'] == 'facebook']
-
+facebook = facebook.reset_index(drop=True)
 
 #%%
 
--- 02_Facebook_Data_Clean.py
+++ 02_Facebook_Data_Clean.py
@@ -14,3 +14,29 @@ import pandas as pd
 
 facebook['content'][0]
 
+#%%
+
+# Assert that all keys in the facebook 'content' column are the same.
+assert sum([facebook['content'][i].keys() != facebook['content'][0].keys() for i in range(10)]) == 0
+
+#%%
+
+content_keys = list(facebook['content'][0].keys())
+# content_keys.remove('reactions')
+
+
+for key in content_keys:
+    facebook[key] = [facebook['content'][i].get(key) for i in range(facebook.shape[0])]
+    
+    
+#%%
+
+reactions_keys = list(facebook['reactions'][0].keys())
+
+#%%
+
+for key in reactions_keys:
+    facebook[key] = [facebook['reactions'][i] for i in range(facebook.shape[0])]
+    
+#%%
+facebook.isnull().sum()
