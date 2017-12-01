e140301a7f6107b253977bd861f3b81f8806683b
bpshaver <benpshaver@gmail.com>
2017-12-01 17:35:32 -0500

    Adding FB data cleaning script

---
+++ 02_Facebook_Data_Clean.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  1 17:32:33 2017
+
+@author: benps
+"""
+
+#%%
+import pandas as pd
+
+#%%
+# This presumes you have just run the 00_Importing_Data.py file, and that you have
+# the 'facebook' dataframe in your environment.
+
+facebook['content'][0]
+
